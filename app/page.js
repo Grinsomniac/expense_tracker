33329d05d5c95c635f35ad57d4d87fc3cce2bb2d
Jason John <johnjason2094@gmail.com>
2024-08-01 13:30:59 -0400

    finalizing day's progress: insert, read, delete from database all working

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, {useState, useEffect} from 'react';
-import { collection, addDoc, getDoc, querySnapshot, query, onSnapshot} from 'firebase/firestore'; 
+import { collection, addDoc, getDoc, querySnapshot, query, onSnapshot, deleteDoc, doc} from 'firebase/firestore'; 
 import {db} from './firebase'
 
 export default function Home() {
@@ -34,9 +34,21 @@ export default function Home() {
         itemsArr.push({...doc.data(), id: doc.id});
       });
       setItems(itemsArr);
+
+      //read total from itemsArr
+      const calculateTotal = () => {
+        const totalPrice = itemsArr.reduce((sum, item) => sum + parseFloat(item.price), 0)
+        setTotal(totalPrice);
+      };
+      calculateTotal();
+      return () => unsubscribe();
     });
   }, []);
+
   //delete item from database
+  const deleteItem = async (id) => {
+    await deleteDoc(doc(db, 'items', id))
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
@@ -71,7 +83,7 @@ export default function Home() {
                   <span className='capitalize'>{item.name}</span>
                   <span>{item.price}</span>
                 </div>
-                <button className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button>
+                <button onClick={() => deleteItem(item.id)}className='ml-8 p-4 border-l-2 border-slate-900 hover:bg-slate-900 w-16'>X</button>
               </li>
             ))}
           </ul>
